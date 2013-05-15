605cde5c3f5da22ef65a6e84158614be03338415
Paolo Zacchi <zacchi.paolo@gmail.com>
2013-05-15 18:26:35 +0200

    create demo directory

--- make.js
+++ make.js
@@ -5,11 +5,6 @@ if (typeof String.prototype.endsWith !== 'function') {
 	};
 }
 
-function printSize(filename) {
-	var result = fs.lstatSync(filename);
-	return Math.round(result.size / 1000) + 'kb';
-}
-
 if (!process.argv[2]){
 	console.log('usage: node make.js album_directory [album_description]');
 	process.exit(1);
@@ -27,10 +22,9 @@ var parameters = null;
 
 // check directories structure
 if (!fs.existsSync(path)){
-    console.log('Path of original photo not exist (' + path + ')!');
+    console.log('Path "' +  path + '" of photos not exist!');
 	process.exit(1);
 }
-
 // load parameters
 console.log('load parameters...');
 try{
@@ -40,7 +34,8 @@ try{
 catch (err){
 	parameters = { 
 		title: process.argv[3] ? process.argv[3] : path,
-		load_images: true, 
+		load_images: true,
+		load_metadata: true,
 		slideshow: 1, 
 		slide_interval: 1000, 
 		transition: 1,
@@ -62,43 +57,46 @@ asyncblock(function (flow) {
 	    	console.log('Process file: ' + files[i].yellow + ' (' + printSize(original_file) + ')');
 	    	// make web version (max 2000 px)
 			var web_file = original_file.replace('.jpg','_web.jpg');
-	    	exec('sips -Z 2000 ' + original_file + ' --out ' + web_file, flow.add());
-	    	result = flow.wait();
-	    	console.log('created web version (' + printSize(web_file) + ')');
+			exec('sips -Z 2000 ' + original_file + ' --out ' + web_file, flow.add());
+			result = flow.wait();
+			console.log('created web version (' + printSize(web_file) + ')');
 	    	// make thumb version (max 200 px)
 			var thumb_file = original_file.replace('.jpg','_thumb.jpg');
 			exec('sips -Z 200 ' + original_file + ' --out ' + thumb_file, flow.add());
 			result = flow.wait();
-	    	console.log('created thumb version (' + printSize(thumb_file) + ')');
-	    	// read metadata
-	    	im.readMetadata(original_file, flow.add());
-	    	var metadata = flow.wait();
+			console.log('created thumb version (' + printSize(thumb_file) + ')');
+
 	    	var title = files[i].replace('.jpg','');
-	    	var meta = '';
-			// description
-	    	if (metadata.exif.imageDescription){
-			    console.log('Description: '.grey + metadata.exif.imageDescription.white);
-			    title = metadata.exif.imageDescription;
+			// metadata
+			if (parameters.load_metadata){
+				// read metadata
+				try{
+					im.readMetadata(original_file, flow.add());
+					var metadata = flow.wait();
+					// description (used for title)
+					if (metadata.exif.imageDescription){
+						console.log('Description: '.grey + metadata.exif.imageDescription.white);
+						title = metadata.exif.imageDescription;
+					}
+					if (metadata.exif.dateTimeOriginal){
+						console.log('Date: '.grey + colors.white(metadata.exif.dateTimeOriginal));
+					}
+					if (metadata.exif.make){
+						console.log('Camera: '.grey + colors.white(metadata.exif.make + ' ' + metadata.exif.model));
+					}
+					if (metadata.exif.isoSpeedRatings){
+						console.log('ISO: '.grey + colors.white(metadata.exif.isoSpeedRatings));
+					}
+					if (metadata.exif.focalLengthIn35mmFilm){
+						console.log('Focal: '.grey + colors.white(metadata.exif.focalLengthIn35mmFilm));
+					}
+				}
+				catch (err){
+					parameters.load_metadata = false;
+				}
 			}
-			if (metadata.exif.dateTimeOriginal){
-			    console.log('Date: '.grey + colors.white(metadata.exif.dateTimeOriginal));
-			    meta += ' <li class="date">' + metadata.exif.dateTimeOriginal + '</li>';
-			}
-			if (metadata.exif.make){
-			    console.log('Camera: '.grey + colors.white(metadata.exif.make + ' ' + metadata.exif.model));
-			    meta += ' <li class="camera">' + metadata.exif.make + ' ' + metadata.exif.model + '</li>';
-			}
-			if (metadata.exif.isoSpeedRatings){
-			    console.log('ISO: '.grey + colors.white(metadata.exif.isoSpeedRatings));
-			    meta += ' <li class="iso">ISO' + metadata.exif.isoSpeedRatings + '</li>';
-			}
-			if (metadata.exif.focalLengthIn35mmFilm){
-			    console.log('Focal: '.grey + colors.white(metadata.exif.focalLengthIn35mmFilm));
-			    meta += ' <li class="focal">' + metadata.exif.focalLengthIn35mmFilm + 'mm</li>';
-			}
-	  	  console.timeEnd('time elapsed');
-	  	  console.log();
-	  	  if (meta) meta = '<ul class="metadata">' + meta + '</ul>';
+			console.timeEnd('time elapsed');
+			console.log();
 	    	parameters.slides[foto] = { image : files[i].replace('.jpg', '_web.jpg'), thumb : files[i].replace('.jpg', '_thumb.jpg'), title: title, url : files[i] };
 		    foto++;
 		}
@@ -113,3 +111,23 @@ asyncblock(function (flow) {
 	fs.writeFile(path + '/index.json', JSON.stringify(parameters, null, 4), 'utf8');
 });
 
+// print size of file (format kb)
+function printSize(filename) {
+	var result = fs.lstatSync(filename);
+	return Math.round(result.size / 1000) + 'kb';
+}
+
+// endWith function for string
+if (typeof String.prototype.endsWith !== 'function') {
+	String.prototype.endsWith = function(suffix) {
+		return this.indexOf(suffix, this.length - suffix.length) !== -1;
+	};
+}
+// resize image, try with sips, then
+function resizeImage(original, resized, max_dimension, callback) {
+	exec('sips -Z ' + max_dimension + ' ' + original + ' --out ' + resized, function (error, stdout, stderr){
+		if (error != null){
+		}
+		callback();
+	});
+}
